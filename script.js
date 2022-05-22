159373d3dd8fe5f7fbfcc41e9c10340a56640e0e
Youngmook Lim <youngmookk@gmail.com>
2022-05-22 16:32:36 +0900

    Add hover coloring functionality and dynamic setting of grid-template-columns through JS

--- script.js
+++ script.js
@@ -2,11 +2,22 @@
 console.log("hola");
 
 const container = document.querySelector(".container");
-
 const divs = document.createElement("div");
 divs.classList.add("divs");
 
-for (let i = 0; i < 16 * 16; i++) {
-  console.log(i);
-  container.appendChild(divs.cloneNode(true));
-}
+const setContainer = function () {
+  const dimension = 30;
+  container.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
+  for (let i = 0; i < dimension ** 2; i++) {
+    // console.log(i);
+    container.appendChild(divs.cloneNode(true));
+  }
+};
+
+setContainer();
+
+container.addEventListener("mouseover", function (e) {
+  console.log(e.target);
+  e.target.closest(".divs").classList.add("hover");
+  // e.target.style.backgroundColor = "purple";
+});
