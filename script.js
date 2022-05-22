a4596293a0f2230306f7cd17b74d625e3ccb345a
Youngmook Lim <youngmookk@gmail.com>
2022-05-22 18:16:52 +0900

    Add button to set dimensions, increasing color gradient per hover, and title

--- script.js
+++ script.js
@@ -2,22 +2,39 @@
 console.log("hola");
 
 const container = document.querySelector(".container");
+const btn = document.querySelector(".btn");
 const divs = document.createElement("div");
 divs.classList.add("divs");
+divs.setAttribute("data-light", 0);
 
-const setContainer = function () {
-  const dimension = 30;
+container.addEventListener("mouseover", function (e) {
+  if (e.target.classList.contains("divs")) {
+    e.target.classList.add("hover");
+
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    console.log(randomColor);
+    const light = e.target.getAttribute("data-light");
+    e.target.setAttribute(`data-light`, +light + 10);
+    e.target.style.backgroundColor = `hwb(205 24% ${
+      20 + +e.target.dataset.light
+    }%)`;
+  }
+});
+
+const setContainer = function (dimension = 30) {
   container.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
+  container.innerHTML = "";
   for (let i = 0; i < dimension ** 2; i++) {
-    // console.log(i);
     container.appendChild(divs.cloneNode(true));
   }
 };
 
 setContainer();
 
-container.addEventListener("mouseover", function (e) {
-  console.log(e.target);
-  e.target.closest(".divs").classList.add("hover");
-  // e.target.style.backgroundColor = "purple";
+btn.addEventListener("click", function () {
+  const dimension = prompt("Enter dimension!");
+  if (dimension > 100) {
+    return alert("Dimension can't be over 100 :(");
+  }
+  setContainer(dimension);
 });
